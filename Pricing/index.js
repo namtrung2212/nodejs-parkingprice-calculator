dcf87618bbca7301fc1d034de8b6111d765e5a63
namtrung2212 <namtrung2212@gmail.com>
2017-12-24 21:17:48 +0700

    no message

--- Pricing/index.js
+++ Pricing/index.js
@@ -33,9 +33,20 @@ server.get('/CalculateBooking', function (req, res) {
     if (req.query.startAt && req.query.minuteQty) {
 
         var startAt = moment.unix(req.query.startAt);
-        var minuteQty = moment(startAt).add(req.query.minuteQty, "m");
+        var endAt = moment(startAt).add(req.query.minuteQty, "m");
+
+        // console.log("order : startAt = " + startAt.utcOffset(7).format("YYYY-MM-DD HH:mm:ss"));
+        // console.log("order : endAt = " + endAt.utcOffset(7).format("YYYY-MM-DD HH:mm:ss"));
 
         var booking = pricing.CalculateBooking(startAt, endAt);
+        // if (booking) {
+
+        //     console.log("booking : startAt = " + (booking.startAt != null ? booking.startAt.utcOffset(7).format("YYYY-MM-DD HH:mm:ss") : null));
+        //     console.log("booking : endAt = " + (booking.endAt != null ? booking.endAt.utcOffset(7).format("YYYY-MM-DD HH:mm:ss") : null));
+        //     console.log("booking : duration = " + Math.floor(booking.minuteQty / 60) + " hours " + (booking.minuteQty % 60) + " minutes");
+        //     console.log("booking : price = " + booking.price); console.log(" ");
+
+        // }
 
         res.json(booking);
 
